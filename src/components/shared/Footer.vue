4c22af3d02695df5634b95d29886f2862ba26ef3
Artur Guitelar <artur.guitelar@gmail.com>
2021-03-14 15:06:08 -0300

    Add: footer buttons to clear data search and get more data.

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,3 +1,41 @@
 <template>
-  <footer class="footer"></footer>
+  <footer class="footer">
+    <div v-show="hasData" class="button-container">
+      <button disabled>Limpar pesquisa</button>
+      <button disabled>Mais registros...</button>
+    </div>
+  </footer>
 </template>
+
+<script>
+export default {
+  computed: {
+    hasData() {
+      return this.$store.getters.getAlbums.length !== 0;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.button-container {
+  width: 360px;
+  margin: 0 auto;
+}
+
+.button-container button {
+  margin: 10px;
+  padding: 10px;
+  background-color: var(--secondary-color);
+  color: var(--gray-text);
+  border: var(--border-default);
+  box-shadow: 6px 6px 3px 0px rgb(0 0 0 / 30%);
+  transition: 0.5s;
+  cursor: pointer;
+}
+
+.button-container button:hover {
+  background-color: var(--main-color);
+  box-shadow: 0 0 9px 3px var(--effect-color);
+}
+</style>
